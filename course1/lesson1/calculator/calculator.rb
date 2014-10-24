8209cb2adf2d18f650d27c3f70f99e03c363bca2
seanlu77 <hailong.lu@gmail.com>
2014-10-24 20:06:55 +0800

    refactor the calculator.rb

--- course1/lesson1/calculator/calculator.rb
+++ course1/lesson1/calculator/calculator.rb
@@ -1,63 +1,74 @@
-# prompt the user to enter number
-def say(msg)
-  print "=> #{msg}"
+puts "Calculator is running ......"
+
+# validate the input 
+def validate_number?(num)
+  if num == "0" || num.to_i != 0 
+    return true
+  else
+    return false
+  end
 end
 
-def reply(msg)
-  puts "---- #{msg}"
-end
-
-#judge the number inputed whether valid
-def is_number?(num)
-  return true if num == '0' || num.to_i != 0
-end
-
-#prompt the user until a valid number is entered
+# get the number  
 def get_number
   begin
     number = gets.chomp
-    puts "#{number} is not a valid number!" unless is_number?(number)
-  end until is_number?(number)
+    puts "It's not a number! input again: " unless validate_number?(number)
+  end until validate_number?(number)
   return number
 end
 
-say"please enter number 1: "
-number1 = get_number
-reply("number 1 is #{number1}")
-
-say("please enter number 2: ")
-number2 = get_number
-reply("number 2 is #{number2}")
-
-begin
-  say("please choose the operator: (1 - add, 2 - subtract, 3 - multiply, 4 - divide): ")
-  operator = gets.chomp
-end until ['1', '2', '3', '4'].include?(operator)
-
-case operator
-  when "1"
-    result = number1.to_f + number2.to_f
-    operator_name = "add"
-    operator_mark = "+"
-  when "2"
-    result = number1.to_f - number2.to_f
-    operator_name = "subtract"
-    operator_mark = "-"
-  when "3"
-    result = number1.to_f * number2.to_f
-    operator_name = "multiply"
-    operator_mark = "*"
-  when "4"
-    if number2.to_i == 0
-      puts "zero can not be divided!"
-    else
-      result = number1.to_f / number2.to_f
-    end
-    operator_name = "divide"
-    operator_mark = "/"
+# get the operator
+def get_operator
+  op_arr = ['+', '-', '*', '/']
+  begin
+    operator = gets.chomp
+    puts "It's not a valid op, choose from #{op_arr}." unless op_arr.include?(operator)
+  end until op_arr.include?(operator)
+  return operator
 end
 
-reply("the operator is #{operator_name}")
+# do the math
+def do_math(num1, num2, op)
+  case op
+  when '+'
+    num1.to_f + num2.to_f
+  when '-'
+    num1.to_f + num2.to_f
+  when '*'
+    num1.to_f * num2.to_f
+  when '/'
+    if num2 == '0'
+      puts "zero can not be divided!"
+    else
+      num1.to_f / num2.to_f
+    end
+  end
+end
 
-puts "------------------------------------"
-puts " the result is: #{number1} #{operator_mark} #{number2} = #{result}"
+# main loop
+begin
+  
+  puts "number 1: "
+  number1 = get_number
+  
+  puts "number 2: "
+  number2 = get_number
+
+  puts "choose operator '+ - * /': "
+  operator = get_operator
+
+  result = do_math(number1, number2, operator)
+
+  puts "#{number1} #{operator} #{number2} = #{result}"
+
+  puts "Do you want to do another calculate?(Y/N)"
+  if ['y', 'Y'].include?(gets.chomp)
+    cal_over = false
+  else
+    cal_over = true
+  end
+
+end until cal_over
+
+puts "Calculator is off."
