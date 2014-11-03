3a66d8ca95a9d17c4bdc45a9aff4fa64507a8ad7
seanlu77 <hailong.lu@gmail.com>
2014-11-03 20:25:30 +0800

    improved AI

--- course1/lesson1/tictactoe_game/ttt.rb
+++ course1/lesson1/tictactoe_game/ttt.rb
@@ -1,109 +1,114 @@
+WINNING_LINES = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], 
+                 ['1', '5', '9'], ['3', '5', '7']]
 
-# draw the game board
-def draw_board(board, player_arr, computer_arr)
+def initialize_board
+  b = {}
+  ('1'..'9').each {|i| b[i] = ''}
+  b
+end
+
+def draw_board(b)
   system 'clear'
   puts
   puts "     |     |"
-  puts "  #{board['1']}  |  #{board['2']}  |  #{board['3']}"
+  puts "  #{b['1']}  |  #{b['2']}  |  #{b['3']}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{board['4']}  |  #{board['5']}  |  #{board['6']}"
+  puts "  #{b['4']}  |  #{b['5']}  |  #{b['6']}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{board['7']}  |  #{board['8']}  |  #{board['9']}"
+  puts "  #{b['7']}  |  #{b['8']}  |  #{b['9']}"
   puts "     |     |"
   puts
-  puts "Player picks: #{player_arr}, Computer picks: #{computer_arr}"
+ # puts "Player picks: #{player_arr}, Computer picks: #{computer_arr}"
 end
 
-def initialize_game(b, player, computer)
-  board = {}
-  player = []
-  computer = []
-  ('1'..'9').each {|number| board[number]=''}
-  player.clear
-  computer.clear
-  return board, player, computer
-end
-
-# player pick a square number
-def player_picks(board, player_arr)
-  print "player picks: "
-  begin
-    player_pick_number = gets.chomp
-    print "square #{player_pick_number} was taken! pick again: " if board[player_pick_number]
-  end until board[player_pick_number].empty?
-  player_arr.push(player_pick_number.to_i)
-  board[player_pick_number] = "X"
-end
-
-# computer pick a square number
-def computer_picks(board, computer_arr)
-  empty_board = board.select {|number, mark| mark.empty?}
-  computer_pick_number = empty_board.keys.sample
-  computer_arr.push(computer_pick_number.to_i)
-  board[computer_pick_number] = "O"
-end
-
-# check whether anyone wins by counting if "XXX" or "OOO" exists based upon the winning_lines
-def win?(board)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-                   [1, 4, 7], [2, 5, 8], [3, 6, 9], 
-                   [1, 5, 9], [3, 5, 7]]
-
-  winning_lines.each do |line|
-    marks = String.new
-    line.each do |number|
-      marks << board[number.to_s]
-    end
-    if marks == "XXX"
-      return ["Player wins!", line]
-      break
-    elsif marks == "OOO"
-      return ["Computer wins!", line]
-      break
-    end
+def check_winner(b)
+  WINNING_LINES.each do |line|
+    return ["Player wins!", line] if b.values_at(*line).count('X') == 3
+    return ["Computer wins!", line] if b.values_at(*line).count('O') == 3
   end
   nil
 end
 
-# check whether the game board is full
-def board_full?(board)
-  empty_board = board.select {|number, mark| mark.empty?}
-  if empty_board.empty?
-    "Board is full!"
+def two_in_a_row(b)
+  WINNING_LINES.each do |line|
+    str = ''
+    line.each do |i|
+      str << b[i]
+    end
+    return line.select {|i| b[i].empty?} if str == 'XX'
+  end
+  nil
+end
+
+def computer_pick(b)
+  if two_in_a_row(b)
+    return two_in_a_row(b)[0]
+  else
+    return empty_board(b).keys.sample
   end
 end
 
-# main loop
-begin
+def empty_board(b)
+  b.select {|k, v| v.empty?}
+end
 
-  board, player_arr, computer_arr = initialize_game(board, player_arr, computer_arr)
+def is_board_full?(b)
+  return true if empty_board(b).empty?
+end
 
-  # draw the empty board when game initialization
-  draw_board(board, player_arr, computer_arr)
+def is_available?(b, pick)
+  return true if b[pick].empty?
+end
 
-  # one game loop
-  loop do
-    player_pick_number = player_picks(board, player_arr)
-    draw_board(board, player_arr, computer_arr)
-    break if win?(board) || board_full?(board)
+puts "Tic Tac Toe Game Starts!"
+puts "what's your name?"
+print '=>'
+player_name = gets.chomp
 
-    computer_picks(board, computer_arr)
-    draw_board(board, player_arr, computer_arr)
-    break if win?(board) || board_full?(board)
+while true
+
+  board = initialize_board
+
+  while true
+
+    draw_board(board)
+
+    while true
+      puts "Pick one: (1 - 9)"
+      print '=>'
+      player_pick = gets.chomp
+      if is_available?(board, player_pick)
+        board[player_pick] = 'X'
+        break
+      end
+    end
+
+    draw_board(board)
+    result = check_winner(board)
+    break if is_board_full?(board) || result
+
+    computer_pick = computer_pick(board)
+    board[computer_pick] = 'O'
+    draw_board(board)
+    result = check_winner(board)
+    break if is_board_full?(board) || result
+
   end
 
-  if result = win?(board)
-    puts "#{result[0]} winning line is: #{result[1]} "
-  elsif result = board_full?(board)
-    puts "#{result}" if result = board_full?(board)
+  if is_board_full?(board) && !result
+    puts "It's a tie!"
+  else
+    puts "#{result[0]}, winning line is #{result[1]}"
   end
 
-  print "Play again?(y/n)"
+  puts "Play again? (y/n)"
+  print '=>'
+  break if gets.chomp != 'y'
 
-end until gets.chomp.downcase != 'y'
+end
 
-puts "Game Over!"
+puts "Game Over, Good Bye!"
