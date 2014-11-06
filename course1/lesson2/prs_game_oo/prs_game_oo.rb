623fa992a4280d55289254452b75fefd7fa0868e
seanlu77 <hailong.lu@gmail.com>
2014-11-06 21:18:43 +0800

    add a Game Class

--- course1/lesson2/prs_game_oo/prs_game_oo.rb
+++ course1/lesson2/prs_game_oo/prs_game_oo.rb
@@ -1,22 +1,31 @@
+class Hand
+  attr_accessor :sign
+
+  def initialize
+    @sign = ''
+  end
+
+end
+
 class Player
-  attr_accessor :name, :choice, :senario
+
+  HANDSIGNS = ['p', 'r', 's']
+  attr_accessor :name, :hand
 
   def initialize(n)
     @name = n
+    @hand = Hand.new
   end
 
-  def check_result(obj)
-    self.senario[self.choice][obj.choice]
-  end
 end
 
 class HumanPlayer < Player
 
-  def get_choice
+  def choose_hand_sign
     while true
       puts "choose from p/r/s:"
       print '=>'
-      break if ['p', 'r','s'].include?(self.choice = gets.chomp)
+      break if HANDSIGNS.include?(self.hand.sign = gets.chomp)
     end
   end
 
@@ -24,41 +33,54 @@ end
 
 class ComputerPlayer < Player
 
-  def get_choice
-    self.choice = ['p', 'r', 's'].sample
+  def choose_hand_sign
+    self.hand.sign = HANDSIGNS.sample
   end
 
 end
 
+class Game
+  SENARIO= {'p' => {'p' => "It's a tie!",
+                    'r' => "paper smushes rock, you win!",
+                    's' => "scissors cut paper, computer win!"},
+
+            'r' => {'p' => "paper smushes rock, computer win!",
+                    'r' => "It's a tie!",
+                    's' => "rock smashes scissors, you win!"},
+
+            's' => {'p' => "scissors cut paper, you win!",
+                    'r' => "rock smashes scissors, computer win!",
+                    's' => "It's a tie!"}
+            }
+
+  attr_accessor :player, :computer
+
+  def initialize
+    @player = HumanPlayer.new('sean')
+    @computer = ComputerPlayer.new('computer')
+  end
+
+  def check_result(player1, player2)
+    SENARIO[player1.hand.sign][player2.hand.sign]
+  end
+
+  def play
+    player.choose_hand_sign
+    computer.choose_hand_sign
+    puts check_result(player, computer)
+  end
+
+end
+
+
+
 while true
 
-  puts "what's your name?"
-  print '=>'
-  player_name = gets.chomp
-  
-  player = HumanPlayer.new(player_name)
-  player.get_choice
-  player.senario = {'p' => {'p' => "It's a tie!",
-                            'r' => "paper smushes rock, you win!",
-                            's' => "scissors cut paper, computer win!"},
-
-                    'r' => {'p' => "paper smushes rock, computer win!",
-                            'r' => "It's a tie!",
-                            's' => "rock smashes scissors, you win!"},
-
-                    's' => {'p' => "scissors cut paper, you win!",
-                            'r' => "rock smashes scissors, computer win!",
-                            's' => "It's a tie!"}
-                    }
-
-  computer = ComputerPlayer.new('computer')
-  computer.get_choice
-
-  puts "#{player.name} chose #{player.choice}, #{computer.name} chose #{computer.choice}"
-  puts player.check_result(computer)
+  Game.new.play
 
   print "play again?(y/n)"
   break if gets.chomp.downcase != 'y'
+  
 end
 
 
